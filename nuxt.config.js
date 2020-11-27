51e1ed292939ba2acf3d8f85cc9b706fb6f7b024
qeude <quentineude@gmail.com>
2020-11-27 13:36:20 +0100

    🐛 Fixed nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,12 +22,14 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
 
+  devModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
