20001b2ea25eade26846d385910449f163540440
qeude <quentineude@gmail.com>
2020-11-27 14:54:37 +0100

    👷🏼‍♂️ Changed number of clusters

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'Nowplayify',
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
+      instances: '2', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
     },
